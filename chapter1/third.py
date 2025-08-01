050e998375030e9f3b73f80b83413716984b1020
Anurag Bhakat <anuragbhakat99@gmail.com>
2025-08-01 21:46:02 +0530

    labelled

--- chapter1/third.py
+++ chapter1/third.py
@@ -1,6 +1,12 @@
 import os 
+
+#specify the directory you want to list
 directory_path ='/'
+
+#list all files and directory in the specific path
 content = os.listdir(directory_path)
+
+#print each files and directory name
 for item in content:
     print(item)
 
