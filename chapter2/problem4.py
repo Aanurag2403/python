71d4bdb48df8aae0f04ed7a008fd4409be699a4c
Anurag Bhakat <anuragbhakat99@gmail.com>
2025-08-02 01:23:56 +0530

    prob4

---
+++ chapter2/problem4.py
@@ -0,0 +1,6 @@
+#use comparision operator to find out whether a gibven variable is greater than 'b' or not. Take a=34 and b=80
+
+a = int(input("enter a number to check: "))
+b = int(input("enter a number2: "))
+
+print("a is greater than b is", a>b)
