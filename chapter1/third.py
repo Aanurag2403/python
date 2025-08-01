9b48f04e05e73cfa12b2eda17d605708774a5102
Anurag Bhakat <anuragbhakat99@gmail.com>
2025-08-01 21:41:08 +0530

    third

---
+++ chapter1/third.py
@@ -0,0 +1,7 @@
+import os 
+directory_path ='/'
+content = os.listdir(directory_path)
+for item in content:
+    print(item)
+
+#write a program to print the content of directory using the os module?
