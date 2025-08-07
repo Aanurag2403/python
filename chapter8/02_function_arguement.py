03d41d305429e8e5f3e50eb762de1f2e69a1871f
Anurag Bhakat <anuragbhakat99@gmail.com>
2025-08-07 23:47:04 +0530

    arggument

---
+++ chapter8/02_function_arguement.py
@@ -0,0 +1,13 @@
+def goodday(name, ending):
+    print("Good day, " + name )
+    print(ending)
+#returning a value
+    return "done"
+
+a = goodday("hari","thankyou")
+print(a)
+
+print("\n")
+
+goodday("rohan","thankyou")
+goodday("rohini","thanks")
