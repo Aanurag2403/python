ffa9c93d9ff3c99121465d55a1710d9f2606840d
Anurag Bhakat <anuragbhakat99@gmail.com>
2025-08-07 23:43:49 +0530

    function

---
+++ chapter8/01_function.py
@@ -0,0 +1,14 @@
+# funstion is a  group of statements performing a specific task.
+
+#defining a function
+def avg():
+    a = int(input("enter a no.: "))
+    b = int(input("enter a no. "))
+    c= int(input("enter your no."))
+
+    average = (a+b+c)/3
+    print(average)
+
+# calling a function
+avg()
+
