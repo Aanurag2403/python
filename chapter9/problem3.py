0d241d29a13a8042eeddfcba8c33348a12c50bdb
Anurag Bhakat <anuragbhakat99@gmail.com>
2025-08-09 02:40:17 +0530

    prob3

---
+++ chapter9/problem3.py
@@ -0,0 +1,13 @@
+# # write a program to generate multiplication table fro 2 to 20 and write it to the different files. place these files in a folder of 13 year old.
+
+def generatetable(n):
+    table = ""
+    for i in range(1,11):
+        table += f"{n} X {i} = {n*i}\n"
+
+    
+    with open(f"tables/table_{n}.txt","w") as f:
+        f.write(table)
+    
+for i in range(2,21):
+    generatetable(i)
