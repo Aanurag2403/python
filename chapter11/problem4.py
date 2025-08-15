4b58ee9c01645560b649a2aecf5ff302398f23cc
Anurag Bhakat <anuragbhakat99@gmail.com>
2025-08-16 02:04:34 +0530

    prob4

---
+++ chapter11/problem4.py
@@ -0,0 +1,22 @@
+# write a class 'Complex' to represent comoplex number along with overloaded operators '+' and '*' which adds and mulptilies them.
+
+class Complex:
+    def __init__(self,r,i):
+        self.r = r
+        self.i = i
+    
+    def __add__(self,c2):
+        return Complex(self.r + c2.r,self.i + c2 .i)
+    
+    def __mul__(self,c2):
+        real_part = self.r *c2.r - self.i *c2.i
+        img_part = self.r * c2.i +self.i *c2.r
+        return Complex(real_part,img_part)
+
+    def __str__(self):
+        return f"{self.r} + {self.i}i"
+c1 = Complex(1,2)
+c2 = Complex(2,4)
+
+print(c1 + c2)
+print(c1*c2)
