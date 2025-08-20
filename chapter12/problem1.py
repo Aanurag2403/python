4be34d640e6a523bd2e7781de8ff9227a43ace5a
Anurag Bhakat <anuragbhakat99@gmail.com>
2025-08-20 09:32:19 +0530

    prob1

---
+++ chapter12/problem1.py
@@ -0,0 +1,21 @@
+# write a program to open three files 1.txt,2.txt,3.txt if any these files are not present, message without existing the program must be printed prompting the same.
+try:  
+    with open("1.txt") as f1:
+        print(f1.read())
+except Exception as e:
+    print(e)
+
+try:      
+    with open('2.txt') as f2:
+        print(f2.read())
+except Exception as e:
+    print(e)
+
+try:    
+    with  open('3.txt')as f3:
+        print(f3.read())
+except Exception as e:
+    print(e)
+print("thankyou")
+
+        
